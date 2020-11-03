59955d6d635a27a84e89ee9938f2c9b000c0c021
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2020-11-03 14:53:25 -0700

    search function able to append user input

--- scripts.js
+++ scripts.js
@@ -1,33 +1,74 @@
 // request API for single day and store in global memory
-var requestToday = api.openweathermap.org/data/2.5/weather?q=Denver&appid=ca363341750e1c79a85ffc9f4699b38a;
 
 // request API for four day and store in global memory
-var requestFuture = pro.openweathermap.org/data/2.5/forecast/hourly?q=Denver&appid=ca363341750e1c79a85ffc9f4699b38a;
+
+
 
 // URL for UV index for Lat and Long
 var latitude = "";
 var longitude = "";
 
 // forloop for 4 day forecast cards
- for(var i = 0; i < requestFuture.length; i++){
+//  for(var i = 0; i < requestFuture.length; i++){
     
- }
+//  }
 
 
-// on click to store data in local storage
-document.getElementById("bth").addEventListener("click", requestToday)
+// append cards
+
+// EVENT LISTENER
+function daily(event) {
+    var input = document.getElementById("input").value
+
+    var requestToday = "https://api.openweathermap.org/data/2.5/weather?q=" + input" + "&appid=ca363341750e1c79a85ffc9f4699b38a";
+    var requestFuture = "pro.openweathermap.org/data/2.5/forecast/hourly?q=" + input + "&appid=ca363341750e1c79a85ffc9f4699b38a";
+
+    // 
 
 
+    event.preventDefault ()
+   var Citystick = document.createElement('p')
+   Citystick.textContent= input
+    document.getElementById("Citystick").appendChild(Citystick)
 
-// fetch request 
-fetch(requestAPI)
+    // input concatination API
+
+    fetch(requestToday)
     .then(function (response) {
-        return response.json();
+        console.log(response)
+        return response.json(); 
+        
     })
     .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-            var listItem = document.createElement("li");
-            listItem.textContent = data[i].html_url;
-            repoList.appendChild(listItem);
-        }
-    });
+        console.log(data)
+        
+    }).catch(function(error){
+        console.log(error)
+    })
+
+
+     
+    }
+document.getElementById("btn").addEventListener("click", daily)
+
+
+// append
+
+
+
+// fetch request and forloop for 5 day forecast
+// fetch(requestAPI)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         for (var i = 0; i < data.length; i++) {
+//             var listItem = document.createElement("li");
+//             listItem.textContent = data[i].html_url;
+//             repoList.appendChild(listItem);
+//         }
+
+//     });
+
+
+
