30fb05c0fbdcc813240be23ae12235e5465464bf
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2020-11-10 18:15:50 -0700

    current day card added

--- scripts.js
+++ scripts.js
@@ -13,35 +13,98 @@ var longitude = "";
     
 //  }
 
+document.getElementById("Citystick").addEventListener('click', function(event){
+    console.log("test")
+    var requestToday = "http://api.openweathermap.org/data/2.5/weather?q=" + event.target.textContent + "&appid=ca363341750e1c79a85ffc9f4699b38a&units=imperial";
+    // var requestFuture = "http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=ca363341750e1c79a85ffc9f4699b38a";
 
+    // Grab info from object and store to variable, then append data.name
+
+
+    event.preventDefault ()
+ 
+    // input concatenation API
+
+    fetch(requestToday)
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        var cityName = document.createElement('h1');
+        var cityDate = document.createElement('h1');
+        var cityTemp = document.createElement('h3');
+
+
+
+        cityName.textContent = data.name
+        cityDate.textContent = data.dt 
+        //moment.js
+        cityTemp.textContent = data.main.temp
+        //convert to F
+
+        var input = document.getElementById("input").value = "";
+        console.log(input)
+        
+        document.getElementById("city").innerHTML = "";
+
+        document.getElementById("city").appendChild(cityName)
+        document.getElementById("city").appendChild(cityDate)
+        document.getElementById("city").appendChild(cityTemp)
+    
+    }).catch(function(error){
+        console.log(error)
+    })
+})
 // append cards
 
 // EVENT LISTENER
 function daily(event) {
     var input = document.getElementById("input").value
 
-    // var requestToday = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=ca363341750e1c79a85ffc9f4699b38a";
-    // var requestFuture = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=ca363341750e1c79a85ffc9f4699b38a";
+    var requestToday = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=ca363341750e1c79a85ffc9f4699b38a&units=imperial";
+    // var requestFuture = "http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=ca363341750e1c79a85ffc9f4699b38a";
 
     // Grab info from object and store to variable, then append data.name
 
 
     event.preventDefault ()
-   var Citystick = document.createElement('p')
-   Citystick.textContent= input
+   var Citystick = document.createElement('button')
+   Citystick.textContent = input
+   Citystick.classList.add("btn")
     document.getElementById("Citystick").appendChild(Citystick)
-
-    // input concatination API
+    
+    // input concatenation API
 
     fetch(requestToday)
     .then(function (response) {
         console.log(response)
-        return response.json(); 
-        
+        return response.json();
     })
     .then(function (data) {
         console.log(data)
+        var cityName = document.createElement('h1');
+        var cityDate = document.createElement('h1');
+        var cityTemp = document.createElement('h3');
+
+
+
+        cityName.textContent = data.name
+        cityDate.textContent = data.dt 
+        //moment.js
+        cityTemp.textContent = data.main.temp
+        //convert to F
+
+        var input = document.getElementById("input").value = "";
+        console.log(input)
         
+        document.getElementById("city").innerHTML = "";
+
+        document.getElementById("city").appendChild(cityName)
+        document.getElementById("city").appendChild(cityDate)
+        document.getElementById("city").appendChild(cityTemp)
+    
     }).catch(function(error){
         console.log(error)
     })
